559097cc99e7d708164ab8a73e3104c19f1714d9
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-10 07:04:22 +0100

    fix: css

--- src/routes/attention-seekers/+page.svelte
+++ src/routes/attention-seekers/+page.svelte
@@ -4,42 +4,25 @@
 	// bounce, flash, pulse, rubberBand, shakeX, shakeY, headShake, swing, tada, wobble, jello, heartBeat
 </script>
 
-<div class="container">
+<div class="space-y-8 flex flex-col justify-center items-center">
 	<h1>Svelte Animate for Svelte 5</h1>
 
-	<Animate animation="backInLeft" trigger="click" duration="0.5s">
+	<Animate animation="bounce" duration="0.5s">
 		<AcademicCap size="50" color="#FF0000" />
 	</Animate>
 
-	<Animate animation="zoomIn" trigger="click" duration="0.5s">
-		<h1>Hello World!</h1>
+	<Animate animation="flash" trigger="click" duration="0.5s">
+		<h1>Click me</h1>
 	</Animate>
 
-	<Animate animation="zoomOutRight" duration="0.5s">
-		<div style="height: 200px; width: 200px; background: #0055f0"></div>
+	<Animate animation="pulse" >
+		<div class="w-40 h-40 bg-blue-200">Pulse</div>
 	</Animate>
 
-	<div class="animation-container">
-		<Animate animation="backInUp" trigger="click" duration="0.5s">
-			<AcademicCap size="50" color="#FF0000" />
-		</Animate>
-	</div>
+	
+	<Animate animation="rubberBand">
+		<AcademicCap size="50" color="#FF0000" />
+	</Animate>
+	
 </div>
 
-<style>
-	.container {
-		margin: 40px;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		/* justify-content: center; */
-		min-height: 100vh;
-	}
-	.animation-container {
-		position: relative;
-		width: 300px;
-		height: 200px;
-		overflow: hidden;
-		background: #f0f0f0;
-	}
-</style>
