e61d1a17a7b9b24a686eb7c82c3995e297dee0dd
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-13 17:02:12 +0100

    docs: fix homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -113,7 +113,7 @@
   </Sidebar>
 
   <div class="w-full max-w-4xl mx-auto p-4 space-y-6">
-    <H1>Single Animatation: {animationName}</H1>
+    <H1>Animatation(s): <span class="text-2xl">{animationName}</span></H1>
     <CodeWrapper>
       <div class="overflow-hidden grid grid-cols-1 w-full gap-4 mb-4">
         <div class="overflow-hidden flex flex-col justify-center border dark:border-gray-600 rounded-lg h-60 min-w-64">
